566a67fd637edd97bf8ed14ad9ea806e67dbea06
XianxinMao <qq1044467857@gmail.com>
2020-08-28 14:08:58 +0800

    upgrade to tf2

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from model import *
 from data import *
-
-#os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+import tensorflow as tf
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
 
 
 data_gen_args = dict(rotation_range=0.2,
@@ -15,8 +23,8 @@ myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,sa
 
 model = unet()
 model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
+model.fit(myGene,steps_per_epoch=300,epochs=10,callbacks=[model_checkpoint])
 
 testGene = testGenerator("data/membrane/test")
-results = model.predict_generator(testGene,30,verbose=1)
+results = model.predict(testGene,30,verbose=1)
 saveResult("data/membrane/test",results)
